c59058f7efab97b3b8c8fd3c2579abd523602c37
lookatme <abujatoekiti@gmail.com>
2025-10-05 17:14:16 +0100

    update

--- first assignment.py
+++ first assignment.py
@@ -1,16 +1,16 @@
-print("My name is P.k, short for Paul Kelly. I'm from Delta state, NIgeria reside in NIgeria and i'm a tech enthusiast")
+# print("My name is P.k, short for Paul Kelly. I'm from Delta state, NIgeria reside in NIgeria and i'm a tech enthusiast")
 
 # assignment 2 & 4
-name = input("please enter your name ")
-nationality = input("please enter your nationality ")
-profession = input("please enter your profesion ")
-print("hello " + name + " all " + nationality + " are welcome to learn python with jude" + " regarless of the fact that you're" + profession + "you are can learn and master python ")
+# name = input("please enter your name ")
+# nationality = input("please enter your nationality ")
+# profession = input("please enter your profesion ")
+# print("hello " + name + " all " + nationality + " are welcome to learn python with jude" + " regarless of the fact that you're" + profession + "you are can learn and master python ")
 
 # assignment (a simple calculator that performs only additional operations) 
-first_number = float(input("enter first number"))
-second_number = float(input("enter sedcond number"))
-result = first_number + second_number
-print (f" the summation to your numbers is  {result}")
-
-
-
+# first_number = float(input("enter first number"))
+# second_number = float(input("enter sedcond number"))
+# result = first_number + second_number
+# print (f" the summation to your numbers is  {result}")
+username=input("please enter your name")
+length = len(username)
+print(length, "\n", username)
