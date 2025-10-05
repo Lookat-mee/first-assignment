a18764a43ff0dd3ab99c6f44410155e852132884
lookatme <abujatoekiti@gmail.com>
2025-10-05 17:57:28 +0100

    welcomed the user

--- first assignment.py
+++ first assignment.py
@@ -1,4 +1,4 @@
-# print("My name is P.k, short for Paul Kelly. I'm from Delta state, NIgeria reside in NIgeria and i'm a tech enthusiast")
+print("My name is P.k, short for Paul Kelly. I'm from Delta state, NIgeria reside in NIgeria and i'm a tech enthusiast")
 
 # assignment 2 & 4
 # name = input("please enter your name ")
@@ -7,10 +7,10 @@
 # print("hello " + name + " all " + nationality + " are welcome to learn python with jude" + " regarless of the fact that you're" + profession + "you are can learn and master python ")
 
 # assignment (a simple calculator that performs only additional operations) 
-# first_number = float(input("enter first number"))
-# second_number = float(input("enter sedcond number"))
-# result = first_number + second_number
-# print (f" the summation to your numbers is  {result}")
+first_number = int(input("enter first number"))
+second_number = int(input("enter sedcond number"))
+result = first_number + second_number
+print (f" the summation to your numbers is  {result}")
 username=input("please enter your name")
 length = len(username)
 print(length, "\n", username)
